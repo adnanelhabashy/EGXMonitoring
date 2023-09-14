166ec28a2d9724ed3c9d2b5d31e69a22db381211
adnan.ahmed <adnan.ahmed@egx.com.eg>
2023-09-14 07:26:22 +0300

    Danger Alert Implemented  and still need some handeling with the normal alert

---
+++ EGXMonitoring/Shared/DTOS/AlertEventData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGXMonitoring.Shared.DTOS
+{
+    public class AlertEventData
+    {
+        public string Message { get; set; } =string.Empty;
+        public int? Severity { get; set; }
+    }
+}
